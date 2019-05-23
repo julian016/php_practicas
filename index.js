11a1f7f1861a3c73f43cf37d3cc2026e9f3ab28c
comkevin <webmaster@gracialab.co>
2019-05-22 19:10:01 -0500

    primer

---
+++ index.js
@@ -0,0 +1,33 @@
+ function valor1() {
+ 		console.log('1');
+  } 
+function valor2(){
+ 		console.log('2')
+ }
+ function valor3(){
+ 		console.log('3')
+ }
+
+var numero2 = (valor2())
+var numero = (valor1())
+
+
+function suma(numero,numero2) 
+{
+	document.write(numero + numero2)
+}
+
+
+
+switch () {
+  case +:
+    
+    [break;]
+  case valor2:
+    //Sentencias ejecutadas cuando el resultado de expresion coincide con valor2
+    [break;]
+  ...
+
+  for (var i = Things.length - 1; i >= 0; i--) {
+  	Things[i]
+  }
